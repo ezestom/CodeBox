46ec7a311c73e041b261621a466b786cb35b6494
ezestom <ezequielstom@gmail.com>
2023-02-20 21:28:53 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,57 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+	const toggle = document.getElementById(toggleId);
+	const nav = document.getElementById(navId);
+
+	toggle.addEventListener("click", () => {
+		// Add show-menu class to nav menu
+		nav.classList.toggle("show-menu");
+
+		// Add show-icon to show and hide the menu icon
+		toggle.classList.toggle("show-icon");
+	});
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/*=============== SHOW DROPDOWN MENU ===============*/
+
+const dropdownItems = document.querySelectorAll(".dropdown__item");
+
+// 1. Select each dropdown item
+dropdownItems.forEach((item) => {
+	const dropdownButton = item.querySelector(".dropdown__button");
+
+	// 2. Select each button click
+	dropdownButton.addEventListener("click", () => {
+		// 7. Select the current show-dropdown class
+		const showDropdown = document.querySelectorAll(".show-dropdown")
+
+		// 5. Call the toggleItem function
+		toogleItem(item);
+
+		// 8. Remove the show-dropdown class from other items
+		if(showDropdown && showDropdown != item){
+			toogleItem(showDropdown)
+			
+		}
+	});
+});
+
+// 3. Create a function to display the dropdown
+const toogleItem = (item) => {
+	// 3.1. Select each dropdown content
+	const dropdownContainer = item.querySelector(".dropdown__container");
+
+	// 6. If the same item contains the show-dropdown class, remove
+	if (item.classList.contains("show-dropdown")) {
+		dropdownContainer.removeAttribute("style");
+		item.classList.remove("show-dropdown");
+	} else {
+		// 4. Add the maximum height to the dropdown content and add the show-dropdown class
+		dropdownContainer.style.height = dropdownContainer.scrollHeight + "px";
+		item.classList.add("show-dropdown");
+	}
+};
+
+/*=============== DELETE DROPDOWN STYLES ===============*/
